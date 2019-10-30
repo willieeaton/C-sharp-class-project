877e3d930fe8d0d997d848a0d6e6b5422ea837cb
Willie Eaton <philip.w.eaton@gmail.com>
2019-10-29 23:45:32 -0400

    Added output, program loop.  My first time using a do-while loop actually.

--- WillieEaton.CodeLou.ExerciseProject/Program.cs
+++ WillieEaton.CodeLou.ExerciseProject/Program.cs
@@ -9,30 +9,48 @@ namespace WillieEaton.CodeLou.ExerciseProject
             Console.WriteLine("Welcome to the Student Registry.  Let\'s create a");
             Console.WriteLine("new student record!");
 
-            Console.WriteLine("Enter the Student ID number.");
-            var studentId = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the student\'s given name.");
-            var firstName = Console.ReadLine();
-            Console.WriteLine("Enter the student\'s family name.");
-            var lastName = Console.ReadLine();
-            Console.WriteLine("Enter the current class in which the student is enrolled.");
-            var className = Console.ReadLine();
-            Console.WriteLine("Enter the class start date.");
-            var startDate = DateTimeOffset.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the last course the student completed.");
-            var lastClassCompleted = Console.ReadLine();
-            Console.WriteLine("Enter the date that course was completed.");
-            var lastClassCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+            bool finishedFlag = false;
 
-            var newStudent = new Student();
-            newStudent.StudentId = studentId;
-            newStudent.FirstName = firstName;
-            newStudent.LastName = lastName;
-            newStudent.ClassName = className;
-            newStudent.StartDate = startDate;
-            newStudent.LastClassCompleted = lastClassCompleted;
-            newStudent.LastClassCompletedOn = lastClassCompletedOn;
+            do
+            {
 
+                Console.WriteLine("Enter the Student ID number.");
+                var studentId = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the student\'s given name.");
+                var firstName = Console.ReadLine();
+                Console.WriteLine("Enter the student\'s family name.");
+                var lastName = Console.ReadLine();
+                Console.WriteLine("Enter the current class in which the student is enrolled.");
+                var className = Console.ReadLine();
+                Console.WriteLine("Enter the class start date.");
+                var startDate = DateTimeOffset.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the last course the student completed.");
+                var lastClassCompleted = Console.ReadLine();
+                Console.WriteLine("Enter the date that course was completed.");
+                var lastClassCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+
+                var newStudent = new Student
+                {
+                    StudentId = studentId,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    ClassName = className,
+                    StartDate = startDate,
+                    LastClassCompleted = lastClassCompleted,
+                    LastClassCompletedOn = lastClassCompletedOn
+                };
+
+                Console.WriteLine("");
+                Console.WriteLine($"Student ID: {newStudent.StudentId}");
+                Console.WriteLine($"Student name: {newStudent.FirstName} {newStudent.LastName}");
+                Console.WriteLine($"Current class: {newStudent.ClassName} started {newStudent.StartDate}");
+                Console.WriteLine($"Previous class: {newStudent.LastClassCompleted} finished {newStudent.LastClassCompletedOn}");
+                Console.WriteLine("");
+                Console.WriteLine("Add another student record?");
+                string response = Console.ReadLine().ToUpper();
+                if (response == "N" || response == "NO")
+                    finishedFlag = true;
+            } while (finishedFlag == false);
         }
     }
 }
