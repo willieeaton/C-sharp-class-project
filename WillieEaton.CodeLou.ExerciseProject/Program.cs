afc3ca14b8375cb4ec4091704bc0c66f08b5d6c8
Willie Eaton <philip.w.eaton@gmail.com>
2019-11-27 19:33:29 -0500

    Finished requirements for project 2

--- WillieEaton.CodeLou.ExerciseProject/Program.cs
+++ WillieEaton.CodeLou.ExerciseProject/Program.cs
@@ -5,30 +5,87 @@ namespace WillieEaton.CodeLou.ExerciseProject
 {
     class Program
     {
+        static List<Student> students = new List<Student>();
+        static Dictionary<string, Student> studentDict = new Dictionary<string, Student>();
+
         static void Main()
         {
+            bool keepLooping = true;
+            Console.WriteLine("Welcome to the student directory.");
+            AddStudents();
+            do
+            {
+                Console.WriteLine("Welcome to the student directory.  Please choose an option.");
+                Console.WriteLine("1. Add students");
+                Console.WriteLine("2. List students");
+                Console.WriteLine("3. Search for student by name");
+                Console.WriteLine("4. Exit directory");
+                int menuChoice = 0;
+                int.TryParse(Console.ReadLine(), out menuChoice);
+                if (menuChoice >= 1 && menuChoice <= 4)
+                {
+                    switch (menuChoice)
+                    {
+                        case 1: 
+                            AddStudents();
+                            break;
+
+                        case 2:
+                            ListStudents();
+                            break;
+
+                        case 3:
+                            SearchStudents();
+                            break;
+
+                        case 4:
+                            keepLooping = false;
+                            break;
+                    }
+
+
+                }
+            } while (keepLooping);
+
+        }
+
+        static void AddStudents()
+        {
+
+            Console.WriteLine("Creating a new student record.");
             var finished = false;
-            var students = new List<Student>();
-
-            Console.WriteLine("Welcome to the Student Registry.  Let\'s create a new student record!");
-
             do
             {
                 Console.WriteLine("");
-                Console.WriteLine("Enter the Student ID number.");
-                var studentId = int.Parse(Console.ReadLine());
+                bool validated = false;
+                int studentId;
+                do
+                {
+                    Console.WriteLine("Enter the Student ID number.");
+                    validated = int.TryParse(Console.ReadLine(), out studentId);
+                } while (!validated);
                 Console.WriteLine("Enter the student\'s first (given) name.");
                 var firstName = Console.ReadLine();
                 Console.WriteLine("Enter the student\'s last (family) name.");
                 var lastName = Console.ReadLine();
                 Console.WriteLine("Enter the current class in which the student is enrolled.");
                 var className = Console.ReadLine();
-                Console.WriteLine("Enter the class start date.");
-                var startDate = DateTimeOffset.Parse(Console.ReadLine());
+                validated = false;
+                DateTimeOffset startDate;
+                do
+                {
+                    Console.WriteLine("Enter the class start date.");
+                    validated = DateTimeOffset.TryParse(Console.ReadLine(), out startDate);
+                } while (!validated);
                 Console.WriteLine("Enter the last course the student completed.");
                 var lastClassCompleted = Console.ReadLine();
-                Console.WriteLine("Enter the date that course was completed.");
-                var lastClassCompletedOn = DateTimeOffset.Parse(Console.ReadLine());
+                validated = false;
+                DateTimeOffset lastCourseCompletedOn;
+                do
+                {
+                    Console.WriteLine("Enter the date that course was completed.");
+                    validated = DateTimeOffset.TryParse(Console.ReadLine(), out lastCourseCompletedOn);
+                } while (!validated);
 
                 var newStudent = new Student
                 {
@@ -38,9 +95,10 @@ namespace WillieEaton.CodeLou.ExerciseProject
                     ClassName = className,
                     StartDate = startDate,
                     LastClassCompleted = lastClassCompleted,
-                    LastClassCompletedOn = lastClassCompletedOn
+                    LastClassCompletedOn = lastCourseCompletedOn
                 };
                 students.Add(newStudent);
+                studentDict.Add(newStudent.FirstName + " " + newStudent.LastName, newStudent);
 
                 Console.WriteLine("");
                 Console.WriteLine("Add another student record?");
@@ -50,14 +108,44 @@ namespace WillieEaton.CodeLou.ExerciseProject
                     finished = true;
                 }
             } while (!finished);
+        }
 
-            foreach(var student in students)
+        static void ListStudents()
+        {
+            foreach (var student in students)
             {
                 Console.WriteLine("");
                 Console.WriteLine($"Student ID: {student.StudentId}");
                 Console.WriteLine($"Student name: {student.FirstName} {student.LastName}");
                 Console.WriteLine($"Current class: {student.ClassName} started {student.StartDate}");
                 Console.WriteLine($"Previous class: {student.LastClassCompleted} finished {student.LastClassCompletedOn}");
+                Console.WriteLine("");
+            }
+        }
+
+        static void ListStudents(Student student)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Student ID: {student.StudentId}");
+            Console.WriteLine($"Student name: {student.FirstName} {student.LastName}");
+            Console.WriteLine($"Current class: {student.ClassName} started {student.StartDate}");
+            Console.WriteLine($"Previous class: {student.LastClassCompleted} finished {student.LastClassCompletedOn}");
+            Console.WriteLine("");
+        }
+
+        static void SearchStudents()
+        {
+            Console.WriteLine("Please enter a student's full name.");
+            var searchName = Console.ReadLine();
+
+            if(studentDict.TryGetValue(searchName, out Student student))
+            {
+                ListStudents(student); 
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} not found in database.");
+                Console.WriteLine("Please check spelling and capitalization.");
             }
         }
     }
