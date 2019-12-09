ebeb90f0ac7ebb217b9490c92bd3d59aa23b9055
Willie Eaton <philip.w.eaton@gmail.com>
2019-12-09 18:40:25 -0500

    Completed through Project 3, also implemented a sort function at student record creation time

--- WillieEaton.CodeLou.ExerciseProject/Program.cs
+++ WillieEaton.CodeLou.ExerciseProject/Program.cs
@@ -1,5 +1,8 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace WillieEaton.CodeLou.ExerciseProject
 {
@@ -11,22 +14,24 @@ namespace WillieEaton.CodeLou.ExerciseProject
         static void Main()
         {
             bool keepLooping = true;
-            Console.WriteLine("Welcome to the student directory.");
-            AddStudents();
+
+            LoadStudents();
+
             do
             {
                 Console.WriteLine("Welcome to the student directory.  Please choose an option.");
                 Console.WriteLine("1. Add students");
                 Console.WriteLine("2. List students");
                 Console.WriteLine("3. Search for student by name");
-                Console.WriteLine("4. Exit directory");
+                Console.WriteLine("4. Search for student by current class");
+                Console.WriteLine("5. Exit directory");
                 int menuChoice = 0;
                 int.TryParse(Console.ReadLine(), out menuChoice);
-                if (menuChoice >= 1 && menuChoice <= 4)
+                if (menuChoice >= 1 && menuChoice <= 5)
                 {
                     switch (menuChoice)
                     {
-                        case 1: 
+                        case 1:
                             AddStudents();
                             break;
 
@@ -39,6 +44,10 @@ namespace WillieEaton.CodeLou.ExerciseProject
                             break;
 
                         case 4:
+                            SearchByClass();
+                            break;
+
+                        case 5:
                             keepLooping = false;
                             break;
                     }
@@ -48,6 +57,7 @@ namespace WillieEaton.CodeLou.ExerciseProject
             } while (keepLooping);
 
         }
+
 
         static void AddStudents()
         {
@@ -63,6 +73,14 @@ namespace WillieEaton.CodeLou.ExerciseProject
                 {
                     Console.WriteLine("Enter the Student ID number.");
                     validated = int.TryParse(Console.ReadLine(), out studentId);
+                    if (validated)
+                    {
+                        if(students.Exists( s => s.StudentId == studentId ))
+                        {
+                            Console.WriteLine("Student already exists in registry with this ID number.");
+                            validated = false;
+                        }
+                    }
                 } while (!validated);
                 Console.WriteLine("Enter the student\'s first (given) name.");
                 var firstName = Console.ReadLine();
@@ -85,6 +103,14 @@ namespace WillieEaton.CodeLou.ExerciseProject
                 {
                     Console.WriteLine("Enter the date that course was completed.");
                     validated = DateTimeOffset.TryParse(Console.ReadLine(), out lastCourseCompletedOn);
+                    if(validated)
+                    {
+                        if (lastCourseCompletedOn >= startDate)
+                        {
+                            Console.WriteLine("Course must have been completed prior to current course start date.");
+                            validated = false;
+                        }
+                    }
                 } while (!validated);
 
                 var newStudent = new Student
@@ -108,6 +134,10 @@ namespace WillieEaton.CodeLou.ExerciseProject
                     finished = true;
                 }
             } while (!finished);
+
+            //once all students have been entered
+            students.Sort();
+            SaveStudents();
         }
 
         static void ListStudents()
@@ -133,20 +163,75 @@ namespace WillieEaton.CodeLou.ExerciseProject
             Console.WriteLine("");
         }
 
+        static void ListStudents(IEnumerable<Student> selectedStudents)
+        {
+            foreach (Student student in selectedStudents)
+            {
+                ListStudents(student);
+            }
+        }
+
         static void SearchStudents()
         {
-            Console.WriteLine("Please enter a student's full name.");
+            Console.WriteLine("Please enter a student's first or last name.");
             var searchName = Console.ReadLine();
-
-            if(studentDict.TryGetValue(searchName, out Student student))
+            var selectedStudents = students.Where(s => s.FirstName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0 || s.LastName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0);
+            if (selectedStudents.Any())
             {
-                ListStudents(student); 
+                ListStudents(selectedStudents);
             }
             else
             {
                 Console.WriteLine($"{searchName} not found in database.");
-                Console.WriteLine("Please check spelling and capitalization.");
+                Console.WriteLine("Please check spelling.");
+            }
+        }
+
+        private static void SearchByClass()
+        {
+            Console.WriteLine("Please enter a class to search for.");
+            var searchName = Console.ReadLine();
+            var selectedStudents = students.Where(s => s.ClassName.IndexOf(searchName, StringComparison.CurrentCultureIgnoreCase) >= 0);
+            if (selectedStudents.Any())
+            {
+                ListStudents(selectedStudents);
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} not found in database.");
+                Console.WriteLine("Please check spelling.");
+            }
+        }
+
+
+        static void SaveStudents()
+        {
+            var serializer = new JsonSerializer();
+            string fileName = "Students.JSON";
+            using (var writer = new StreamWriter(fileName))
+            using (var jsonWriter = new JsonTextWriter(writer))
+            {
+                serializer.Serialize(jsonWriter, students);
+            }
+
+        }
+
+        static void LoadStudents()
+        {
+            string fileName = "Students.JSON";
+            if (File.Exists(fileName))
+            {
+                var serializer = new JsonSerializer();
+                using (var reader = new StreamReader(fileName))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    students = serializer.Deserialize<List<Student>>(jsonReader);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Students database not found on disk.  Will be created upon adding students.");
             }
         }
     }
-} 
+}
--- WillieEaton.CodeLou.ExerciseProject/Student.cs
+++ WillieEaton.CodeLou.ExerciseProject/Student.cs
@@ -1,15 +1,20 @@
 using System;
 
 namespace WillieEaton.CodeLou.ExerciseProject
-{ 
-    public class Student
+{
+    public class Student : IComparable<Student>
     {
-    	public int StudentId { get; set; }
-    	public string FirstName  { get; set; }
-    	public string LastName  { get; set; }
-    	public string ClassName { get; set; }
-    	public DateTimeOffset StartDate { get; set; }
-    	public string LastClassCompleted { get; set; }
-    	public DateTimeOffset LastClassCompletedOn { get; set; }
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+       public DateTimeOffset StartDate { get; set; }
+        public string LastClassCompleted { get; set; }
+        public DateTimeOffset LastClassCompletedOn { get; set; }
+
+        public int CompareTo(Student that)
+        {
+            return this.StudentId.CompareTo(that.StudentId);
+        }
     }
 }
